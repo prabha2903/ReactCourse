3351cd36403a45f9ce7af7a67fbd94998a62c4fd
Prabhacod <prabha29070607@gmail.com>
2026-06-14 11:51:49 +0530

    Build a todo list

---
+++ todo-app/src/App.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import "./App.css";
+import InputContainer from "./components/InputContainer";
+import TodoContainer from "./components/TodoContainer";
+function App() {
+  const [inputVal,setInputVal] = useState('');
+  const [todos,setTodos] = useState([])
+ function writeTodo(e){
+  setInputVal(e.target.value)
+ }
+ function addTodo(){
+    if(inputVal!=''){
+      setTodos((prevTodos)=>[...prevTodos,inputVal])
+      setInputVal('')
+    }
+ } 
+ function delTodo(todoIndex){
+    setTodos((prevTodos)=>prevTodos.filter((prevTodos,prevTodosIndex)=>{
+      return prevTodosIndex != todoIndex;
+    })
+
+    )
+ }
+  return (
+    <main>
+      <h2>To Do List</h2>
+      <InputContainer inputVal={inputVal} writeTodo={writeTodo} addTodo={addTodo}/>
+      <TodoContainer todos={todos} delTodo={delTodo}/>
+    </main>
+  )
+}
+
+export default App
